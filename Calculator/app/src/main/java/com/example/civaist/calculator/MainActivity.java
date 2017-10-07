a5e229ef451befa1c0814e675a6235ddaadd1fd1
shivapbhusal <civabhusal@gmail.com>
2017-10-07 16:47:49 -0400

    new design added

--- Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
@@ -20,68 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addButton=(Button)findViewById(R.id.add_button);
-        subButton=(Button)findViewById(R.id.sub_button);
-        mulButton=(Button)findViewById(R.id.mul_button);
-        divButton=(Button)findViewById(R.id.div_button);
-        num1=(EditText)findViewById(R.id.numText1);
-        num2=(EditText)findViewById(R.id.numText2);
-        result=(EditText)findViewById(R.id.numTextResult);
-        addNumbers();
-        subNumbers();
-        mulNumbers();
-        divNumbers();
+
+
     }
-
-    public void addNumbers()
-    {
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                float a=Float.parseFloat(num1.getText().toString());
-                float b=Float.parseFloat(num2.getText().toString());
-                String sum=Float.toString(a+b);
-                result.setText(sum);
-
-            }
-        });
-    }
-    public void subNumbers()
-    {
-        subButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                float a=Float.parseFloat(num1.getText().toString());
-                float b=Float.parseFloat(num2.getText().toString());
-                String sum=Float.toString(a-b);
-                result.setText(sum);
-
-            }
-        });
-    }
-    public void mulNumbers(){
-        mulButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                float a=Float.parseFloat(num1.getText().toString());
-                float b=Float.parseFloat(num2.getText().toString());
-                String prod=Float.toString(a*b);
-                result.setText(prod);
-
-            }
-        });
-    }
-    public void divNumbers(){
-        divButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                float a=Float.parseFloat(num1.getText().toString());
-                float b=Float.parseFloat(num2.getText().toString());
-                String div=Float.toString(a/b);
-                result.setText(div);
-
-            }
-        });
-    }
-
 }
