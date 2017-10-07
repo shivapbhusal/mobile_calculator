1c4987bd0621a045e7e2b2488684e8f87573b733
shivapbhusal <civabhusal@gmail.com>
2017-10-07 19:02:55 -0400

    got the case statement working

--- Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
@@ -1,23 +1,33 @@
 package com.example.civaist.calculator;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
-    Button addButton;
-    Button subButton;
-    Button mulButton;
-    Button divButton;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public Button addButton;
+    public Button subButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addButton = (Button) findViewById(R.id.btn_add);
+        subButton = (Button) findViewById(R.id.btn_sub);
+        addButton.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.btn_add:
+                Toast.makeText(MainActivity.this, "button1", Toast.LENGTH_LONG).show();
+                break;
+        }
 
     }
+
 }
