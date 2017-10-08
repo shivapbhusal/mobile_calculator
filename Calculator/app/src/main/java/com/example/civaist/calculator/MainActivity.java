afff08a58e6b6b5724165ec3c6738d75e60534fa
shivapbhusal <civabhusal@gmail.com>
2017-10-07 23:49:17 -0400

    text input screen added

--- Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
@@ -3,6 +3,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public Button addButton;
     public Button subButton;
+    public TextView textInput;
+    public TextView textOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +20,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         addButton = (Button) findViewById(R.id.btn_add);
         subButton = (Button) findViewById(R.id.btn_sub);
+        textInput=(TextView) findViewById(R.id.edit_input);
+        textOutput=(TextView) findViewById(R.id.edit_output);
         addButton.setOnClickListener(this);
+        subButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.btn_add:
-                Toast.makeText(MainActivity.this, "button1", Toast.LENGTH_LONG).show();
+                textInput.setText("+");
                 break;
+            case R.id.btn_sub:
+                textInput.setText("-");
         }
 
     }
