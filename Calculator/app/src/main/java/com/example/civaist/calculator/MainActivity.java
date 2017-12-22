80ebb9015e413da5f7fa1195322a26d1ebee1a83
shivapbhusal <civabhusal@gmail.com>
2017-12-21 22:39:20 -0500

    edit layout, make ready for refactoring

--- Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
@@ -2,67 +2,21 @@ package com.example.civaist.calculator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     public static String expression="";
-    public Button addButton;
-    public Button subButton;
-    public Button prodButton;
-    public Button zeroButton;
-    public Button oneButton;
-    public Button twoButton;
-    public Button threeButton;
-    public Button fourButton;
-    public Button fiveButton;
-    public Button sixButton;
-    public Button sevenButton;
-    public Button eightButton;
-    public Button nineButton;
-    public Button equalButton;
-    public TextView textInput;
-    public TextView textOutput;
+    public TextView textInput=(TextView) findViewById(R.id.edit_input);
+    public TextView textOutput=(TextView) findViewById(R.id.edit_output);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addButton = (Button) findViewById(R.id.btn_add);
-        subButton=(Button) findViewById(R.id.btn_sub);
-        prodButton=(Button)findViewById(R.id.btn_product);
-        equalButton=(Button)findViewById(R.id.btn_equal);
-        zeroButton=(Button)findViewById(R.id.btn_zero);
-        oneButton=(Button)findViewById(R.id.btn_one);
-        twoButton=(Button)findViewById(R.id.btn_two);
-        threeButton=(Button)findViewById(R.id.btn_three);
-        fourButton=(Button)findViewById(R.id.btn_four);
-        fiveButton=(Button)findViewById(R.id.btn_five);
-        sixButton=(Button)findViewById(R.id.btn_six);
-        sevenButton=(Button)findViewById(R.id.btn_seven);
-        eightButton=(Button)findViewById(R.id.btn_eight);
-        nineButton=(Button)findViewById(R.id.btn_nine);
-        textInput=(TextView) findViewById(R.id.edit_input);
-        textOutput=(TextView) findViewById(R.id.edit_output);
-        addButton.setOnClickListener(this);
-        subButton.setOnClickListener(this);
-        prodButton.setOnClickListener(this);
-        equalButton.setOnClickListener(this);
-        zeroButton.setOnClickListener(this);
-        oneButton.setOnClickListener(this);
-        twoButton.setOnClickListener(this);
-        threeButton.setOnClickListener(this);
-        fourButton.setOnClickListener(this);
-        fiveButton.setOnClickListener(this);
-        sixButton.setOnClickListener(this);
-        sevenButton.setOnClickListener(this);
-        eightButton.setOnClickListener(this);
-        nineButton.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
+    public void buttonClick(View view) {
         switch(view.getId()) {
             case R.id.btn_add:
                 expression=expression.concat("+");
