fff5645a0eea9c158d7d7c3d437830fd38dc98f9
shivapbhusal <civabhusal@gmail.com>
2018-02-11 14:37:44 -0500

    add changes

--- Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
@@ -7,8 +7,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
     public static String expression;
-    public TextView textInput;
-    public TextView textOutput;
+    TextView textInput;
+    TextView textOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
