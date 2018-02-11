7098e19f66792f79c79375de89c5d70acd409524
shivapbhusal <civabhusal@gmail.com>
2018-02-11 13:55:03 -0500

    play around

--- Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
@@ -2,16 +2,32 @@ package com.example.civaist.calculator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static String expression;
+    public static TextView textInput;
+    public static TextView textOutput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        expression = "";
+        textInput = (TextView) findViewById(R.id.edit_input);
+        textOutput = (TextView) findViewById(R.id.edit_output);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setInputOutput();
     }
-    public void onClickButton(View view){
-        Toast.makeText(MainActivity.this,"Hello World", Toast.LENGTH_LONG).show();
+
+    public static void onClickButton(View view) {
+
+    }
+
+    public static void setInputOutput()
+    {
+        textInput.setText("Hello");
+        textOutput.setText("Abc");
     }
 }
 
