d29d70c8f51339aaf56b5d2d63b720b5cabbd350
shivapbhusal <civabhusal@gmail.com>
2018-02-11 12:24:54 -0500

    cheanup and revert to initial layout

--- Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
@@ -1,90 +1,13 @@
 package com.example.civaist.calculator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
-
 
 public class MainActivity extends AppCompatActivity {
-    public static String expression="";
-    public TextView textInput=(TextView) findViewById(R.id.edit_input);
-    public TextView textOutput=(TextView) findViewById(R.id.edit_output);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void buttonClick(View view) {
-        switch(view.getId()) {
-            case R.id.btn_add:
-                expression=expression.concat("+");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_sub:
-                expression=expression.concat("-");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_divide:
-                expression=expression.concat("/");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_product:
-                expression=expression.concat("*");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_zero:
-                expression=expression.concat("0");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_one:
-                expression=expression.concat("1");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_two:
-                expression=expression.concat("2");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_three:
-                expression=expression.concat("3");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_four:
-                expression=expression.concat("4");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_five:
-                expression=expression.concat("5");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_six:
-                expression=expression.concat("6");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_seven:
-                expression=expression.concat("7");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_eight:
-                expression=expression.concat("8");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_nine:
-                expression=expression.concat("9");
-                textInput.setText(expression);
-                break;
-            case R.id.btn_equal:
-                Evaluate myEvaluate=new Evaluate();
-                float myresult=myEvaluate.evalExp(expression);
-                String result = Float.toString(myresult);
-                textOutput.setText(result);
-                break;
-
-        }
-
-    }
-
 }
 
 
