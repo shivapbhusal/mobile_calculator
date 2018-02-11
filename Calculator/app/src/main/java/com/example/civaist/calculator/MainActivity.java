ad73c343be33c59e21176475bbc8a7a3847dad3b
shivapbhusal <civabhusal@gmail.com>
2018-02-11 14:54:30 -0500

    complete refactor

--- Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
@@ -6,28 +6,85 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
-    public static String expression;
+    public static String expression="";
     TextView textInput;
     TextView textOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        expression = "";
-        textInput = (TextView) findViewById(R.id.edit_input);
-        textOutput = (TextView) findViewById(R.id.edit_output);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setInputOutput();
+        textInput = (TextView) findViewById(R.id.edit_input);
+        textOutput = (TextView) findViewById(R.id.edit_output);
     }
 
     public void onClickButton(View view) {
-        Toast.makeText(MainActivity.this,"Hello one !", Toast.LENGTH_LONG).show();
+        switch(view.getId()) {
+            case R.id.btn_add:
+                expression=expression.concat("+");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_sub:
+                expression=expression.concat("-");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_divide:
+                expression=expression.concat("/");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_product:
+                expression=expression.concat("*");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_zero:
+                expression=expression.concat("0");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_one:
+                expression=expression.concat("1");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_two:
+                expression=expression.concat("2");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_three:
+                expression=expression.concat("3");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_four:
+                expression=expression.concat("4");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_five:
+                expression=expression.concat("5");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_six:
+                expression=expression.concat("6");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_seven:
+                expression=expression.concat("7");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_eight:
+                expression=expression.concat("8");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_nine:
+                expression=expression.concat("9");
+                textInput.setText(expression);
+                break;
+            case R.id.btn_equal:
+                Evaluate myEvaluate=new Evaluate();
+                float myResult=myEvaluate.evalExp(expression);
+                String result = Float.toString(myResult);
+                textOutput.setText(result);
+                break;
+        }
     }
 
-    public void setInputOutput()
-    {
-        textInput.setText("Hello");
-    }
 }
 
 
