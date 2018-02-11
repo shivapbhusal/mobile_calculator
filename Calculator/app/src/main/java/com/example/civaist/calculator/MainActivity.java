c34a61e726908307d88e92ff522d10fa531108bf
shivapbhusal <civabhusal@gmail.com>
2018-02-11 15:05:48 -0500

    delete uncecessary lines

--- Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
@@ -3,7 +3,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
     public static String expression="";
