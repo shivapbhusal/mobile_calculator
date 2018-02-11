2192130d54b9de2f19a7e553bd6ecd6682252035
shivapbhusal <civabhusal@gmail.com>
2018-02-11 12:35:55 -0500

    add onclick function to all buttons

--- Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/civaist/calculator/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.civaist.calculator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClickButton(View view){
+        Toast.makeText(MainActivity.this,"Hello World", Toast.LENGTH_LONG).show();
     }
 }
 
